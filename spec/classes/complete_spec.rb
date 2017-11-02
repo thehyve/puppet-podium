3301e12bcb32523cb16699263fdccf19869e139b
Gijs Kant <gijs@thehyve.nl>
2017-11-02 11:29:20 +0100

    Add support for CentOS, Ubuntu.

--- spec/classes/complete_spec.rb
+++ spec/classes/complete_spec.rb
@@ -1,15 +1,19 @@
 require 'spec_helper'
 describe 'podium::complete' do
-  context 'with default values for all parameters' do
-    let(:node) { 'test2.example.com' }
-    it { should compile.and_raise_error(/No database password specified. Please configure podium::gateway_db_password/) }
-  end
-  context 'with proper values set' do
-    let(:node) { 'test.example.com' }
-    it { is_expected.to create_class('podium::user') }
-    it { is_expected.to create_class('podium::config') }
-    it { is_expected.to create_class('podium::artefacts') }
-    it { is_expected.to create_class('podium::database') }
-    it { is_expected.to create_class('podium::services') }
+  on_supported_os.each do |os, facts|
+    context "with default values for all parameters on #{os}" do
+      let(:facts) { facts }
+      let(:node) { 'test2.example.com' }
+      it { should compile.and_raise_error(/No database password specified. Please configure podium::gateway_db_password/) }
+    end
+    context "with proper values set on #{os}" do
+      let(:facts) { facts }
+      let(:node) { 'test.example.com' }
+      it { is_expected.to create_class('podium::user') }
+      it { is_expected.to create_class('podium::config') }
+      it { is_expected.to create_class('podium::artefacts') }
+      it { is_expected.to create_class('podium::database') }
+      it { is_expected.to create_class('podium::services') }
+    end
   end
 end
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
 describe 'podium' do
-  context 'with default values for all parameters' do
-    let(:node) { 'test2.example.com' }
-    it { should compile.and_raise_error(/No database password specified. Please configure podium::gateway_db_password/) }
-  end
-  context 'with proper values set' do
-    let(:node) { 'test.example.com' }
-    it { is_expected.to create_class('podium') }
+  on_supported_os.each do |os, facts|
+    context "with default values for all parameters on #{os}" do
+      let(:facts) { facts }
+      let(:node) { 'test2.example.com' }
+      it { should compile.and_raise_error(/No database password specified. Please configure podium::gateway_db_password/) }
+    end
+    context "with proper values set on #{os}" do
+      let(:facts) { facts }
+      let(:node) { 'test.example.com' }
+      it { is_expected.to create_class('podium') }
+    end
   end
 end
